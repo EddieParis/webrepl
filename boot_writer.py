7133fd564f7825c5d5c2474efff1d29d097aa6f7
Olivier Ortigues <oortigues@verimatrix.com>
2018-02-16 14:51:49 +0100

    Add subsitution as a mode

--- boot_writer.py
+++ boot_writer.py
@@ -16,12 +16,13 @@ import argparse
 class NoAckError(Exception):
     pass
 
-def send_python(serial, src_name, args_dict, check=False):
+def send_python(serial, src_name, args_dict, subsitute, check=False):
     with open(src_name, "rt") as input_file:
         while True:
             # ~ line=input_file.readln()
             line=input_file.readline()
-            line = line.format(**args_dict)
+            if subsitute:
+                line = line.format(**args_dict)
             if line == "":
                 break
             elif check:
@@ -42,7 +43,7 @@ parser = argparse.ArgumentParser(description='Micropython serial file writer.')
 
 parser.add_argument('-s', '--serial', metavar='serial', type=str, help='serial device', nargs=1, required=True)
 
-parser.add_argument('-p', '--param', metavar='param', type=str, help='parameter', nargs='*')
+parser.add_argument('-p', '--param', metavar='param', type=str, help='parameter', nargs='*',)
 
 parser.add_argument('-b', '--baudrate', metavar='baudrate', type=int, nargs=1, default=[115200],
                 help='baurate for serial, default 115200')
@@ -53,17 +54,24 @@ args = parser.parse_args()
 
 print args
 
-transfered_args = { 'dest_name':args.file[0]}
+transfered_args = {'dest_name':args.file[0]}
 
 if args.param:
     for param in args.param:
+        if param.find('=') == -1:
+            print("Parameters must match pattern xxx=yyy")
+            exit(-1)
         name, value = param.split('=')
         transfered_args.update( {name:value} )
 
-send_python(serial, "input.py", transfered_args, True)
-send_python(serial, args.file, transfered_args, True)
+send_python(serial, "input.py", transfered_args, True, True)
+try:
+    send_python(serial, args.file, transfered_args, args.param!=None, True)
+except ValueError:
+    print("Error, it seems your downloaded file have { or } that are not name substitution (dict or json for instance), then you can not have parameters")
+    exit(-1)
 
 serial = serial.Serial(port=args.serial[0], baudrate=args.baudrate[0], timeout=1)
-send_python(serial, "input.py", transfered_args)
-send_python(serial, args.file, transfered_args)
+send_python(serial, "input.py", transfered_args, True)
+send_python(serial, args.file, transfered_args, args.param!=None)
 serial.close()
